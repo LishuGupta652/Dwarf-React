cdc79dc8f667e8ae2a5d53d04c0584cb4a2c6d5f
lishu gupta <lishugupta652@gmail.com>
2020-03-01 02:18:51 +0530

    template updated

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Logo from "../images/icon.png";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="nav-container">
+          <nav>
+            <img src={Logo} alt="logo" className="logo" />
+
+            <ul className="nav-links">
+              <li className="nav-link-active">Home</li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="content">
+          <div className="left">
+            <h1>Dwarf</h1>
+            <h2>Building Smart </h2>
+            <h2>
+              India<span className="dot">.</span>
+            </h2>
+          </div>
+          <div className="right"></div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
