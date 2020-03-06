1c5407f156d9aafd6fbccead5e7bec4e09db4186
lishu gupta <lishugupta652@gmail.com>
2020-03-06 13:32:14 +0530

    prev

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,9 @@ const Products = () => {
   return (
     <div className="products">
       <div className="container">
+        <h1 style={{ textAlign: "center", color: "#e67e22" }}>Our Products</h1>
+      </div>
+      <div className="container">
         <div className="poster">
           <h1>Container</h1>
         </div>
