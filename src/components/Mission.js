f7ef2f80c2dd2c1cb558e9e8d40e83756b5a2a81
lishu gupta <lishugupta652@gmail.com>
2020-03-14 18:18:51 +0530

    products added

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,16 +1,21 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Mission = () => {
   return (
     <>
       <div className="mission">
         <div className="mission-container">
-          <h1>Our mission</h1>
-          <p>
-            To Help India Achieve its smart City Objective, by Providing
-            Innovation, Best in Class Consulting, Solutions and services. We
-            shall make a joy for all stakeholders to work with us.
-          </p>
+          <ScrollAnimation animateIn="fadeIn">
+            <h1>Our mission</h1>
+          </ScrollAnimation>
+          <ScrollAnimation animateIn="fadeIn">
+            <p>
+              To Help India Achieve its smart City Objective, by Providing
+              Innovation, Best in Class Consulting, Solutions and services. We
+              shall make a joy for all stakeholders to work with us.
+            </p>
+          </ScrollAnimation>
         </div>
       </div>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,23 @@ import Header from "../components/Header";
 import About from "../components/About";
 import Mission from "../components/Mission";
 import Products from "../components/Products";
+
+import ScrollAnimation from "react-animate-on-scroll";
+
 const Home = () => {
   return (
     <>
       <Header title="Home" />
-      <About title="About" />
-      <Mission />
-      <Products />
 
+      <ScrollAnimation animateIn="fadeIn">
+        <About />
+      </ScrollAnimation>
+
+      <div className="md-space" />
+      <Mission />
+
+      <div className="md-space" />
+      <Products />
       <div className="space"></div>
     </>
   );
