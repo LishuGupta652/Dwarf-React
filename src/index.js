1fa9406e86636644d31f55cc4079d70488c8e13f
lishu gupta <lishugupta652@gmail.com>
2020-03-28 18:22:34 +0530

    router fixed HashRouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
